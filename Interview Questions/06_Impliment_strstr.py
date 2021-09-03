a0f3173e72533c16681cd6447bdf7d15275dd3ca
Keyur Chaniyara <86070861+Keyurchaniyara@users.noreply.github.com>
2021-09-03 23:53:39 +0530

    Add files via upload

---
+++ Interview Questions/06_Impliment_strstr.py
@@ -0,0 +1,7 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if 0 <= len(haystack) and len(needle) <= (5*pow(10,4)):
+            return haystack.find(needle)
+        
+s = Solution()
+s.strStr("hello","ll")
