373fa00958e2c0de9d75c6535008204855c49545
Keyur Chaniyara <86070861+Keyurchaniyara@users.noreply.github.com>
2021-09-07 21:23:03 +0530

    Add files via upload

---
+++ Interview Questions/08_longest_common_prefix.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestCommonPrefix(self, arr: List[str]) -> str:
+        arr.sort()
+        result = ""
+        for i in range(len(arr[0])):
+            if arr[0][i] == arr[-1][i]:
+                result += arr[0][i]
+            else:
+                break
+        return result
+
+arr = ["flower","flow","flight"]    
+s = Solution()
+s.longestCommonPrefix(arr)
